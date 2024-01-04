8be352f2f3ee3fe9841c59c78c1722942d42e04d
wskk267 <114842313+wskk267@users.noreply.github.com>
2024-01-04 09:08:55 +0800

    Update head.h

--- 交通咨询系统/交通咨询系统/head.h
+++ 交通咨询系统/交通咨询系统/head.h
@@ -1,1 +1,25 @@
 #pragma once
+typedef struct infoCar{
+  double distance;
+  double cost;
+  double time;
+}InfoCar;
+typedef struct infoTrain{
+  double cost;
+  double time;
+}InfoTrain;
+typedef struct info{
+  string start;
+  string end;
+  InfoCar infoCar;
+  InfoTrain infoTrain;
+}Info;
+typedef struct node{
+  Info info;
+  Node *next;
+  Node *pre;
+}Node;
+Node *InitNode();
+void CheckNode(Node* head,string start,string end);
+void CheckList(Node* head);
+void DeleteNode(Node* head);
