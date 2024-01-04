287db5597d517ce508072e689732a39ad95518dc
mingtian2004 <155437288+mingtian2004@users.noreply.github.com>
2024-01-04 10:23:53 +0800

    Update function.c

--- 交通咨询系统/交通咨询系统/function.c
+++ 交通咨询系统/交通咨询系统/function.c
@@ -4,3 +4,23 @@
 #include <math.h>
 #include <stdlib.h>
 #include "head.h"
+void CheckNode(Node* head,char start[],char end[])
+{
+  Node* p;
+  p=head;
+  while(p!=NULL)
+  {
+    if(!(strcmp(start,(p->info).start))&&!(strcmp(end,(p->info).end)))
+    {
+      printf("%s %s 私家车:距离:%lf,花费:%lf,时间:%lf;火车:花费:lf,时间:lf.",start,end,(p->info).infoCar.distance,(p->info).infoCar.cost,(p->info).infoCar.time,(p->info).infoTrain.cost,(p->info).infoTrain.time);
+    }
+    else
+    {
+      p=p->next;
+    }
+  }
+  if(p==NULL)
+  {
+    printf("没有相关记录");
+  }  
+}
