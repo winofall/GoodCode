a452c2a08c6a62c91c884b40ca2206d04301fa46
winofall <110169527+winofall@users.noreply.github.com>
2024-01-04 22:25:25 +0800

    Update head.h

--- 交通咨询系统/交通咨询系统/head.h
+++ 交通咨询系统/交通咨询系统/head.h
@@ -1,31 +1,31 @@
 #pragma once
 #define V 10
-typedef struct infoCar{
-  double distance;
-  double cost;
-  double time;
+typedef struct infoCar {
+	double distance;
+	double cost;
+	double time;
 }InfoCar;
-typedef struct infoTrain{
-  double cost=0;
-  double time;
+typedef struct infoTrain {
+	double cost = 0;
+	double time;
 }InfoTrain;
-typedef struct info{
-  char start[V];
-  char end[V];
-  InfoCar infoCar;
-  InfoTrain infoTrain;
+typedef struct info {
+	char start[V];
+	char end[V];
+	InfoCar infoCar;
+	InfoTrain infoTrain;
 }Info;
-typedef struct node{
-  Info info;
-  Node *next;
-  Node *pre;
+typedef struct node {
+	Info info;
+	Node* next;
+	Node* pre;
 }Node;
-Node *InitNode();
-void CheckNode(Node* head,char start[],char end[]);//查询该结点信息
-void CheckList(Node* head,int start,int end);
-void DeleteNode(Node* head,char start[],char end[]);//删除该节点
-void CreateNode(Node* head,char start[],char end[]);//从文件中获取信息，建立结点
-void GetMoney(Node* head,char start[],char end[]);//从给的起始点，在链表中找出cost最小的路径
-void GetDis(Node* head,char start[],char end[]);//从给的起始点，在链表中找出distance最小的路径
-void GetTime(Node* head,char start[],char end[]);//从给的起始点，在链表中找出time最小的路径
-void AddNode(Node* head,char start[],char end[],double CarDistance,double CarCost,double Cartime,double TrainCost,double TrainTime);//更新与start，end的结点的数据
+Node* InitNode();
+void CheckNode(Node* head, char start[], char end[]);//查询该结点信息
+void CheckList(Node* head, int start, int end);
+void DeleteNode(Node* head, char start[], char end[],const char* filename);//删除信息
+void CreateNode(Node* head, char start[], char end[], const char* filename);//从文件中获取信息，建立结点
+void GetMoney(Node* head, char start[], char end[]);//从给的起始点，在链表中找出cost最小的路径
+void GetDis(Node* head, char start[], char end[]);//从给的起始点，在链表中找出distance最小的路径
+void GetTime(Node* head, char start[], char end[]);//从给的起始点，在链表中找出time最小的路径
+void AddNode(Node* head, char start[], char end[], double CarDistance, double CarCost, double Cartime, double TrainCost, double TrainTime);//更新与start，end的结点的数据
