6c01f11ae6f11e54cd3182b0e4a93aff2d84633d
wskk267 <114842313+wskk267@users.noreply.github.com>
2024-01-04 10:54:22 +0800

    Update test.c

--- 交通咨询系统/交通咨询系统/test.c
+++ 交通咨询系统/交通咨询系统/test.c
@@ -3,68 +3,74 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-#include "head.h"
-#include "function.c"
 int main()
 {
-  char password[100]={'6','6','6','6'},Up[100];
-  int n;
-  printf("--------欢迎使用交通资讯系统--------\n");
-while(n){
-  printf("-----------请选择以下功能-----------\n");
-  printf("-------------0.退出系统-------------\n");
-  printf("-----------1.查询最短路径-----------\n");
-  printf("-----------2.查询最少金额-----------\n");
-  printf("-----------3.查询最短时间-----------\n");
-  printf("----------4.进入管理员系统----------\n");
-  scanf("%d",&n);
-  switch(n){
-    case 0:
-      printf("退出成功\n");
-      break;
-    case 1://查询最短路径;
-      break;
-    case 2://查询最少金额;
-      break;
-    case 3://查询最短时间;
-      break;
-    case 4://进入管理员系统;
-    scanf("%s",Up);
-    if(strcmp(Up,password)){
-      printf("进入成功\n");
-      while(n){
-          printf("-----------请选择以下功能-----------\n");
-          printf("-------------1.增加信息-------------\n");
-          printf("-----------2.查询所有信息-----------\n");
-          printf("-------------3.减少信息-------------\n");
-          printf("----------4.退出管理员系统----------\n");
-          scanf("%d",&n);
-          switch(n){
-            case 0:
-              printf("退出模式成功\n");
-              break;
-            case 1://增加信息;
-              break;
-            case 2://查询所有信息;
-              break;
-            case 3://减少信息;
-              break;
-            case 4://退出管理员系统;   
-              break;
-            default:
-              printf("输入错误");
-              break;
+    char password[100] = {'6', '6', '6', '6'}, Up[100];
+    int n;
+    printf("--------欢迎使用交通资讯系统--------\n");
+    while (n)
+    {
+        printf("-----------请选择以下功能-----------\n");
+        printf("-------------0.退出系统-------------\n");
+        printf("-----------1.查询最短路径-----------\n");
+        printf("-----------2.查询最少金额-----------\n");
+        printf("-----------3.查询最短时间-----------\n");
+        printf("----------4.进入管理员系统----------\n");
+        scanf("%d", &n);
+        switch (n)
+        {
+        case 0:
+            printf("退出成功\n");
+            break;
+        case 1: // 查询最短路径;
+            break;
+        case 2: // 查询最少金额;
+            break;
+        case 3: // 查询最短时间;
+            break;
+        case 4: // 进入管理员系统;
+            printf("请输入密码:");
+            scanf("%s", Up);
+            if (strcmp(Up, password) == 0)
+            {
+                printf("进入成功\n");
+                while (n)
+                {
+                    printf("-----------请选择以下功能-----------\n");
+                    printf("-------------1.增加信息-------------\n");
+                    printf("-----------2.查询所有信息-----------\n");
+                    printf("-------------3.减少信息-------------\n");
+                    printf("----------0.退出管理员系统----------\n");
+                    scanf("%d", &n);
+                    switch (n)
+                    {
+                    case 0:
+                        printf("退出模式成功\n");
+                        break;
+                    case 1: // 增加信息;
+                        break;
+                    case 2: // 查询所有信息;
+                        break;
+                    case 3: // 减少信息;
+                        break;
+                    case 4: // 退出管理员系统;
+                        break;
+                    default:
+                        printf("输入错误\n");
+                        break;
+                    }
+                }
             }
-      }
-   } else{
-      printf("密码错误\n");
+            else
+            {
+                printf("密码错误\n");
+            }
+            break;
+        default:
+            printf("输入错误");
+            break;
+        }
     }
-      break;
-    default:
-      printf("输入错误");
-      break;
-}
-}
-system("pause");
-return 0;
+    system("pause");
+    return 0;
 }
