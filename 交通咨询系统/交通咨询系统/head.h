f5cb139348799a5faac338e2fa85588c663faa06
mingtian2004 <155437288+mingtian2004@users.noreply.github.com>
2024-01-05 08:29:02 +0800

    Update head.h

--- 交通咨询系统/交通咨询系统/head.h
+++ 交通咨询系统/交通咨询系统/head.h
@@ -29,7 +29,7 @@ Node* InitNode();
 void CheckNode(Node* head, char start[], char end[]);//查询该结点信息
 void CheckList(Node* head, int start, int end);
 void DeleteNode(Node* head, char start[], char end[],const char* filename);//删除信息
-void CreateNode(Node* head, char start[], char end[], const char* filename);//从文件中获取信息，建立结点
+void CreateNode(Node* head,  const char* filename);//从文件中获取信息，建立结点
 void GetMoney(Node* head, char start[], char end[]);//从给的起始点，在链表中找出cost最小的路径
 void GetDis(Node* head, char start[], char end[]);//从给的起始点，在链表中找出distance最小的路径
 void GetTime(Node* head, char start[], char end[]);//从给的起始点，在链表中找出time最小的路径
