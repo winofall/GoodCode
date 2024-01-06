a301164cafc8e52ccdb978f415a45233d1e06fba
winofall <110169527+winofall@users.noreply.github.com>
2024-01-06 21:18:37 +0800

    Update head.h

--- 交通咨询系统/交通咨询系统/head.h
+++ 交通咨询系统/交通咨询系统/head.h
@@ -25,16 +25,14 @@ typedef struct graph {
 	Node* head;
 	double value[100][100];
 	char place[100][20];
-	int nums;
-	int path[100];
 }MGraph;
 Node* InitNode();
 void CheckNode(Node* head, char start[], char end[]);//查询该结点信息
 void CheckAllNode(Node* head);//查询所有结点信息
 void CheckList(Node* head, int start, int end);
-void DeleteNode(Node* head, char start[], char end[],const char* filename);//删除信息
-void CreateNode(Node* head,  const char* filename);//从文件中获取信息，建立结点
+void DeleteNode(Node* head, char start[], char end[], const char* filename);//删除信息
+void CreateNode(Node* head, const char* filename);//从文件中获取信息，建立结点
 void GetMoney(Node* head, char start[], char end[]);//从给的起始点，在链表中找出cost最小的路径
 void GetDis(Node* head, char start[], char end[]);//从给的起始点，在链表中找出distance最小的路径
 void GetTime(Node* head, char start[], char end[]);//从给的起始点，在链表中找出time最小的路径
-void AddNode(const char* filename, const char* start, const char* end, double CarDistance, double CarCost, double Cartime, double TrainCost, double TrainTime)//更新文件
+void AddNode(const char* filename, const char* start, const char* end, double CarDistance, double CarCost, double Cartime, double TrainCost, double TrainTime);//更新
