514bcca49bcc19e2a9fe4adf391e576fe5303dcb
wskk267 <114842313+wskk267@users.noreply.github.com>
2024-01-09 00:50:28 +0800

    Update head.h

--- 交通咨询系统/交通咨询系统/head.h
+++ 交通咨询系统/交通咨询系统/head.h
@@ -34,12 +34,12 @@ typedef struct graph {
 Node* InitNode();
 void CheckNode(Node* head, char start[], char end[]);//查询该结点信息
 void CheckAllNode(Node* head);//查询所有结点信息
-void DeleteNode(Node** head, char start[], char end[], const char* filename);//删除信息
+void DeleteNode(MGraph* m, Node** head, char start[], char end[], const char* filename);//删除信息
 int CreateNode(MGraph *m,Node** head, const char* filename);//从文件中获取信息，建立结点
 void GetMoney(MGraph* m, char start[], char end[]);//从给的起始点，在链表中找出cost最小的路径
 void GetDis(MGraph* m, char start[], char end[]);//从给的起始点，在链表中找出distance最小的路径
 void GetTime(MGraph* m, char start[], char end[]);//从给的起始点，在链表中找出time最小的路径
-void AddNode(MGraph m,Node** head, const char* filename, const char* start, const char* end, double CarDistance, double CarCost, double Cartime, double TrainCost, double TrainTime);//更新
+void AddNode(MGraph* m,Node** head, const char* filename, const char* start, const char* end, double CarDistance, double CarCost, double Cartime, double TrainCost, double TrainTime);//更新
 void Dijkstra(MGraph m, int i, int j, int path[], Value vk[]);
 void AddPlace(MGraph *m, char place[]);//添加地点进m图
-void PrintPath(MGraph* m, char start[], char end[], int vk);
+void PrintPath(MGraph* m, char start[], char end[], int vk);//打印路径
